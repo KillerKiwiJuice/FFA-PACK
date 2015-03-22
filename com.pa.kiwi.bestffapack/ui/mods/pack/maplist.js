13afd831b235bd63a67cf5bfa930dd2b3628f55e
KillerKiwiJuice <eoin@comcast.net>
2015-03-22 13:08:51 -0400

    release

---
+++ com.pa.kiwi.bestffapack/ui/mods/pack/maplist.js
@@ -0,0 +1,6 @@
+cShareSystems.load_pas("Kiwi's FFA Map Pack", [
+	"coui://ui/mods/pack/systems/jungle_platforms (less than 11 players only).pas",
+	"coui://ui/mods/pack/systems/pretty_much insanity.pas",
+	"coui://ui/mods/pack/systems/rna_- next gen ffa (less than 11 players only).pas",
+	"coui://ui/mods/pack/systems/tug-o'-war_(less than 11 players only).pas"
+]);
