eff94a452d6077c56854f540ce0c77a83944f60f
KillerKiwiJuice <eoin@comcast.net>
2015-04-28 21:03:13 -0400

    update

--- com.pa.kiwi.bestffapack/ui/mods/pack/maplist.js
+++ com.pa.kiwi.bestffapack/ui/mods/pack/maplist.js
@@ -2,5 +2,7 @@ cShareSystems.load_pas("Kiwi's FFA", [
 	"coui://ui/mods/pack/systems/jungle_platforms_(less than 11 players only).pas",
 	"coui://ui/mods/pack/systems/pretty_much_insanity.pas",
 	"coui://ui/mods/pack/systems/rna_-_next_gen_ffa.pas",
-	"coui://ui/mods/pack/systems/tug-o'-war_(less than 11 players only).pas"
+	"coui://ui/mods/pack/systems/tug-o'-war_(less than 11 players only).pas",
+	"coui://ui/mods/pack/systems/20p_ffa madness.pas",
+	"coui://ui/mods/pack/systems/no_csg lava planet.pas"
 ]);
---
+++ com.pa.kiwi.bestffapack/ui/mods/pack/systems/no_csg lava planet.pas
@@ -0,0 +1,90 @@
+{
+    "name": "No CSG Lava Planet",
+    "planets": [
+        {
+            "name": "Asgard",
+            "mass": 10000,
+            "position_x": -200,
+            "position_y": -22600,
+            "velocity_x": 148.73228454589844,
+            "velocity_y": -1.3162084817886353,
+            "required_thrust_to_move": 0,
+            "starting_planet": true,
+            "planet": {
+                "seed": 388254560,
+                "radius": 1100,
+                "heightRange": 35,
+                "waterHeight": 33,
+                "waterDepth": 50,
+                "temperature": 100,
+                "metalDensity": 103,
+                "metalClusters": 166,
+                "biomeScale": 50,
+                "biome": "lava",
+                "symmetryType": "none",
+                "symmetricalMetal": false,
+                "symmetricalStarts": false,
+                "numArmies": 2,
+                "landingZonesPerArmy": 0,
+                "landingZoneSize": 0
+            },
+            "planetCSG": [
+                {
+                    "weight": [
+                        1,
+                        1,
+                        1,
+                        1
+                    ],
+                    "spec": "/pa/terrain/lava/brushes/lava_volcano_06.json",
+                    "proj": "BP_Bend",
+                    "transform": [
+                        -0.12854354083538055,
+                        -0.9768511056900024,
+                        -0.31004247069358826,
+                        -331.5628967285156,
+                        0.48936715722084045,
+                        -0.3330514132976532,
+                        0.8464541435241699,
+                        905.2075805664062,
+                        -0.9004912376403809,
+                        -0.04155126214027405,
+                        0.5042589902877808,
+                        539.26025390625
+                    ],
+                    "op": "BO_Add",
+                    "rotation": 1.87579345703125,
+                    "scale": [
+                        1.032902717590332,
+                        1.032902717590332,
+                        1.032902717590332
+                    ],
+                    "height": 1104.5977783203125,
+                    "position": [
+                        -331.5628356933594,
+                        905.2073974609375,
+                        539.26025390625
+                    ],
+                    "weightHard": false,
+                    "weightScale": [
+                        1,
+                        1,
+                        1
+                    ],
+                    "mirrored": false,
+                    "twinId": 0,
+                    "flooded": false,
+                    "pathable": false,
+                    "mergeable": false,
+                    "no_features": false,
+                    "biomeColor": [
+                        0,
+                        0,
+                        0,
+                        1
+                    ]
+                }
+            ]
+        }
+    ]
+}
